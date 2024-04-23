eb226c5ed265d7aee4f003170a66630988fdd847
Wayllw <135237073+Wayllw@users.noreply.github.com>
2024-04-23 23:19:27 +0100

    1

---
+++ ytDownload.py
@@ -0,0 +1,26 @@
+from pytube import YouTube
+import tkinter as tk
+
+
+def Download():
+    link = title_entry.get()
+    yt = YouTube(link)
+
+    video = yt.streams.get_highest_resolution()
+    video.download("C:/YtDownload")
+
+    window.destroy()
+
+
+window = tk.Tk()
+window.title("Youtube Downloader")
+label_entry = tk.Label(window,text="Link: ")
+label_entry.grid(row=0,column=0)
+title_entry = tk.Entry(window)
+title_entry.grid(row=0,column=1)
+button = tk.Button(window,text="Download",command=Download)
+button.grid(row=0,column=2)
+button.configure(bg="#445566", fg="white")
+label_entry.configure(bg="#445566", fg="white")
+window.configure(bg="#445566")
+window.mainloop()
